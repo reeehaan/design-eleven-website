9e5b8f000adce18b8c7db65f943980049f09757d
reeehaan <rehan221104@gmail.com>
2026-05-12 16:51:29 +0530

    feat : add cloudinary

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
       // { protocol: "https", hostname: "cdn.sanity.io" },
-      // { protocol: "https", hostname: "res.cloudinary.com" },
     ],
   },
 };
